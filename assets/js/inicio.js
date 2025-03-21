b0d1f7f97627cce2a92d0f60009858eda4019efd
SolArgentinoTV2025 <traplegionrecords2020@gmail.com>
2025-03-21 20:17:41 -0300

    mas

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -120,3 +120,7 @@ function cargarProductos() {
         }
     });
 }
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
